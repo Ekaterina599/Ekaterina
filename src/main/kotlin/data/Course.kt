ea0bc3231280ef9eb4c49985475c55c2b6d62ee8
Ekaterina599 <82459656+Ekaterina599@users.noreply.github.com>
2022-11-23 18:11:19 +0600

    Add files via upload

---
+++ src/main/kotlin/data/Course.kt
@@ -0,0 +1,21 @@
+package data
+
+typealias CourseId = String
+typealias Grade = Int
+
+data class Course(
+    val name: String,
+    val students: Array<StudentId>,
+    val marked: Array<Boolean> = Array(students.size) { false },
+    val grades: Array<Grade> = Array(students.size){1}
+) {
+    val id: CourseId
+        get() = name
+}
+
+val courseList =
+    arrayOf(
+        "Math",
+        "Phys",
+        "Story"
+    ).map { Course(it, studentList.map { it.id }.toTypedArray()) }
---
+++ src/main/kotlin/redux/action.kt
@@ -0,0 +1,37 @@
+import data.Course
+import data.CourseId
+import data.Student
+import data.StudentId
+import data.Grade
+import redux.RAction
+
+
+open class CourseAction: RAction
+open class StudentAction: RAction
+open class ModeAction: RAction
+
+class MarkStudent(
+    val courseId: CourseId,
+    val studentId: StudentId
+) : CourseAction()
+
+class AddStudent(
+    val student: Student
+): StudentAction()
+
+class AddCourse(
+    val course: Course
+): CourseAction()
+
+class AddStudentToCourse(
+    val courseId: CourseId,
+    val studentId: StudentId
+): CourseAction()
+
+class AddGrade(
+    val courseId: CourseId ,
+    val studentId: StudentId,
+    val grade: Grade
+): CourseAction()
+
+class ChangeMode() : ModeAction()
